91ca92ca43ad4dc68f99604af7988ac8511f4099
YourNewUsername <yournewemail@example.com>
2026-06-18 21:36:12 +0100

    Fix GitHub OAuth callback URL for Render

--- config/passport.js
+++ config/passport.js
@@ -14,7 +14,10 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "/auth/github/callback",
+      callbackURL:
+        process.env.NODE_ENV === "production"
+          ? "https://cse341-project2-htyh.onrender.com/auth/github/callback"
+          : "http://localhost:3000/auth/github/callback",
     },
     (accessToken, refreshToken, profile, done) => {
       return done(null, profile);
